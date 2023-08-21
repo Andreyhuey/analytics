6d57717f63e9d0c267126b78fca746d78bf152c0
Andreyhuey <andreyhuey777@gmail.com>
2023-08-21 10:41:45 +0100

    Features component made responsive on all devices

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -61,33 +61,37 @@ const Features = () => {
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row justify-center items-center gap-4 py-9">
-        <div className="lg:max-w-[410px]">
-          <p className="font-[700] text-[40px] text-[#172755]">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-x-4 gap-y-1 py-9">
+        <div className="lg:max-w-[410px] order-2 lg:order-1 text-center lg:text-left">
+          <p className="font-[700] md:text-[40px] text-[30px]  text-[#172755]">
             Automated Reports & Widget Alerts
           </p>
-          <p className="text-[#8794BA]">
+          <p className="text-[#8794BA] pt-1">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum
             nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam
             vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor
             pulvinar risus pulvinar sit amet.
           </p>
         </div>
-        <Image src={screen1} alt="desc" className="w-[600px] h-[400px]" />
+        <Image
+          src={screen1}
+          alt="desc"
+          className="w-[600px] h-[400px] order-1 lg:order-2"
+        />
       </div>
 
-      <div className="flex flex-col md:flex-row justify-start items-center gap-4 py-9">
+      <div className="flex flex-col lg:flex-row justify-start items-center gap-x-4 gap-y-1 py-9">
         <Image
           src={screen2}
           alt="desc"
-          className="w-[600px] h-[400px] lg:mr-7"
+          className="lg:w-[600px] h-[400px] lg:mr-7"
         />
 
-        <div className="lg:max-w-[410px]">
-          <p className="font-[700] text-[40px] text-[#172755]">
+        <div className="lg:max-w-[410px] text-center lg:text-left">
+          <p className="font-[700] md:text-[40px] text-[30px] text-[#172755]">
             Fully customizable to address your needs
           </p>
-          <p className="text-[#8794BA]">
+          <p className="text-[#8794BA] pt-1">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum
             nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam
             vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor
@@ -96,19 +100,23 @@ const Features = () => {
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row justify-between items-center gap-4 py-9">
-        <div className="lg:max-w-[410px]">
-          <p className="font-[700] text-[40px] text-[#172755]">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-x-4 gap-y-1 py-9">
+        <div className="lg:max-w-[410px] order-2 lg:order-1 text-center lg:text-left ">
+          <p className="font-[700] md:text-[40px] text-[30px] text-[#172755]">
             Pre-built Dashboard Templates
           </p>
-          <p className="text-[#8794BA]">
+          <p className="text-[#8794BA] pt-1">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum
             nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam
             vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor
             pulvinar risus pulvinar sit amet.
           </p>
         </div>
-        <Image src={screen3} alt="desc" className="w-[600px] h-[400px]" />
+        <Image
+          src={screen3}
+          alt="desc"
+          className="w-[600px] h-[400px] order-1 lg:order-2"
+        />
       </div>
     </div>
   );
