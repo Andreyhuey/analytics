71a7e6361b1003e0aa9f3e5761e66248b5720d23
Andreyhuey <andreyhuey777@gmail.com>
2023-08-21 11:33:01 +0100

    Footer component complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import logo from "../assets/logo.svg";
+import Image from "next/image";
+import { BiLogoFacebook, BiLogoTwitter, BiLogoInstagram } from "react-icons/bi";
 
 const Footer = () => {
   return (
-    <div>
-      <></>
+    <div className=" px-4 lg:px-40">
+      <div className="border border-t-[#2A407C] border-b-0 border-r-0 border-l-0 py-14">
+        <div className="flex flex-col md:flex-row md:justify-between items-center text-center gap-y-10  ">
+          <Image src={logo} alt="analytics logo" />
+          <div className="flex flex-col md:flex-row text-[#8794BA] gap-x-9 gap-y-3">
+            <p>Products</p>
+            <p>Pricing Plans</p>
+            <p>FAQ</p>
+            <p>Blog</p>
+          </div>
+          <div className="flex items-center justify-center gap-5 text-[#8794BA] text-[24px] cursor-pointer">
+            <BiLogoFacebook />
+            <BiLogoTwitter />
+            <BiLogoInstagram />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
