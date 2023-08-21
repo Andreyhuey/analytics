b72953c435431e54e5e45bcb0bbc3377b02ea7e4
Andreyhuey <andreyhuey777@gmail.com>
2023-08-21 09:11:47 +0100

    Features component completed

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -4,10 +4,13 @@ import React from "react";
 import monitor from "../assets/monitoring-Icon.svg";
 import widget from "../assets/desktop-Icon.svg";
 import rocket from "../assets/rocket-Icon.svg";
+import screen1 from "../assets/screen-01.svg";
+import screen2 from "../assets/screen-02.svg";
+import screen3 from "../assets/screen-03.svg";
 
 const Features = () => {
   return (
-    <div className=" h-screen my-2 py-12 px-4 lg:px-40">
+    <div className="my-2 py-12 px-4 lg:px-40">
       <div className="text-center">
         <h1
           className="text-[#172755] text-center font-[700] text-[40px]
@@ -56,6 +59,56 @@ const Features = () => {
             nisi aliquet volutpat.
           </p>
         </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-center items-center gap-4 py-9">
+        <div className="lg:max-w-[410px]">
+          <p className="font-[700] text-[40px] text-[#172755]">
+            Automated Reports & Widget Alerts
+          </p>
+          <p className="text-[#8794BA]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum
+            nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam
+            vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor
+            pulvinar risus pulvinar sit amet.
+          </p>
+        </div>
+        <Image src={screen1} alt="desc" className="w-[600px] h-[400px]" />
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-start items-center gap-4 py-9">
+        <Image
+          src={screen2}
+          alt="desc"
+          className="w-[600px] h-[400px] lg:mr-7"
+        />
+
+        <div className="lg:max-w-[410px]">
+          <p className="font-[700] text-[40px] text-[#172755]">
+            Fully customizable to address your needs
+          </p>
+          <p className="text-[#8794BA]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum
+            nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam
+            vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor
+            pulvinar risus pulvinar sit amet.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-between items-center gap-4 py-9">
+        <div className="lg:max-w-[410px]">
+          <p className="font-[700] text-[40px] text-[#172755]">
+            Pre-built Dashboard Templates
+          </p>
+          <p className="text-[#8794BA]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum
+            nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam
+            vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor
+            pulvinar risus pulvinar sit amet.
+          </p>
+        </div>
+        <Image src={screen3} alt="desc" className="w-[600px] h-[400px]" />
       </div>
     </div>
   );
