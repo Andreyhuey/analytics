424b0f3358bc1c0897153aefd23aaa68ac0a3c3e
Andreyhuey <andreyhuey777@gmail.com>
2023-08-26 20:57:56 +0100

    Nav for mobile devices completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 //
 import logo from "../assets/subtract.svg";
-import backgroundImage from "../assets/background-top.svg";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 import mainScreen from "../assets/main-screen.svg";
 import Link from "next/link";
 
 const Header = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => setNav(!nav);
+
   return (
     <div className="relative overflow-hidden bg-[url('../assets/background-top.svg')] bg-no-repeat bg-cover">
       <div className="relative z-10 lg:py-12 md:py-8 py-4 px-4 lg:px-40">
@@ -45,6 +51,66 @@ const Header = () => {
             </div>
           </div>
         </nav>
+        {/* nav component for phones */}
+        <nav>
+          <div className="flex justify-between items-center md:hidden">
+            <Link className="flex gap-3 items-center cursor-pointer" href="/">
+              <Image src={logo} alt="Analytics logo" />
+              <h1 className=" font-extrabold text-[18px] ">ANALYTICS</h1>
+            </Link>
+
+            {nav ? (
+              <HiOutlineMenuAlt3
+                className="text-white text-[40px]"
+                onClick={handleNav}
+              />
+            ) : (
+              <AiOutlineClose
+                className="text-white text-[40px]"
+                onClick={handleNav}
+              />
+            )}
+          </div>
+        </nav>
+
+        <div
+          className={` ${
+            !nav
+              ? "ease-in duration-300 transition"
+              : "ease-out duration-300 transition"
+          } `}
+        >
+          {!nav ? (
+            <ul className="text-[#8794BA] font-[500] text-[16px] gap-y-8 pl-[50px] pt-6 flex flex-col items-start ">
+              <li>
+                <Link href="/">Products</Link>
+              </li>
+              <li>
+                <Link href="/">Pricing</Link>
+              </li>
+              <li>
+                <Link href="/">FAQ</Link>
+              </li>
+              <li>
+                <Link href="/">Blog</Link>
+              </li>
+              <li>
+                <Link href="/" className="text-[#8794BA] font-[400]">
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link href="/" className="text-[#8794BA] font-[400]">
+                  <button className="border border-[#8794BA] rounded-3xl px-8 py-2 hover:bg-[#EF2A82] hover:text-white hover:ease-in hover:duration-300 hover:transition">
+                    Sign Up
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
 
       <div className="relative z-10 pl-4 lg:pl-40">
